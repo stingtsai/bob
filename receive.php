6c23e481d0e16b69e3f6fc0f50f2f8e8ca0bbcdb
stingtsai <47617870+stingtsai@users.noreply.github.com>
2019-02-14 11:13:44 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -2,6 +2,6 @@
  $json_str = file_get_contents('php://input');
  $json_obj = json_decode($json_str);
  
- $myfile = fopen("log.txt", "w+") or die("Unable to open file");
+ $myfile = fopen("log.txt", "w+") or die("Unable to open file!");
  fwrite($myfile, "\xEF\xBB\xBF\".$json_str);
 ?>

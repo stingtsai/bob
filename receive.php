4ad7d3082d382ea2fbed749e1307679837a1bf95
stingtsai <47617870+stingtsai@users.noreply.github.com>
2019-02-14 10:57:29 +0800

    Create receive.php

---
+++ receive.php
@@ -0,0 +1,7 @@
+<?php
+ $json_str = file_get_contents('php://input');
+ $json_obj = json_decode(json_str);
+ 
+ $myfile = fopen("log.txt"), "w+") or die("Unable to open file");
+ fwrite($myfile, "\xEF\xBB\xBF\".$json_str);
+?>
